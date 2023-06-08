56c9cab443dfe6b993a8642e2bc235c477e40f65
R Avenash <avenash97@gmail.com>
2023-06-08 11:48:48 +0530

    Added current time

--- fivetran_poc/models/example/transformed_sheets.sql
+++ fivetran_poc/models/example/transformed_sheets.sql
@@ -4,5 +4,5 @@ with source_data as (
     select string from `sadaindia-tvm-poc-de`.google_sheets.poc_sheets
 )
 
-select UPPER(string) as upper_string
+select UPPER(string) as upper_string, CURRENT_TIME() as now
 from source_data
