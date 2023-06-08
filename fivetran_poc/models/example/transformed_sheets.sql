e2ef706c2d12b94c89b0c1008956b402530c3be9
Pradeep Gurbani <pradeep.gurbani@sada.com>
2023-06-08 11:59:15 +0530

    Updated model

--- fivetran_poc/models/example/transformed_sheets.sql
+++ fivetran_poc/models/example/transformed_sheets.sql
@@ -1,8 +1,8 @@
-{{ config(materialized='view') }}
+-- {{ config(materialized='view') }}
 
 with source_data as (
-    select string from `sadaindia-tvm-poc-de`.google_sheets.poc_sheets
+    select string, timestamp from `sadaindia-tvm-poc-de`.google_sheets.poc_sheets
 )
 
-select UPPER(string) as upper_string, CURRENT_TIME() as now
+select UPPER(string) as upper_string, timestamp as time_stamp
 from source_data
