7a31b266e081a807be1dfa4432cabbc3db8781fb
gurbanipradeep1503 <111963888+gurbanipradeep1503@users.noreply.github.com>
2023-06-15 15:40:30 +0530

    Updated transformed_sheets model with the query

--- fivetran_poc/models/example/transformed_sheets.sql
+++ fivetran_poc/models/example/transformed_sheets.sql
@@ -6,16 +6,15 @@
 }}
 
 with source_data as (
-    select first_name, last_name, job_title, salary, _fivetran_synced from {{source('poc','poc_sheets')}}
+    select first_name, last_name, job_title, email, salary, _fivetran_synced from {{source('poc','poc_sheets')}}
 )
 
 select Concat(first_name, " ", last_name) as full_name, job_title, salary, _fivetran_synced
 from source_data
-where salary > 9500
 
 {% if is_incremental() %}
 
   -- this filter will only be applied on an incremental run
-  where _fivetran_synced > (select max(_fivetran_synced) from {{ this }})
+  where _fivetran_synced > (select max(_fivetran_synced) from {{ this }}) and salary > 9500
 
 {% endif %}
